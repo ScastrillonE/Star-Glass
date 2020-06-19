6be156ecdaa969b916e6606a94c2e5e56b0d0188
SantiagoCE <santiagocastrillon.ep@gmail.com>
2020-06-19 17:09:47 -0500

    "Adjustment responsive"

--- core/views.py
+++ core/views.py
@@ -12,6 +12,7 @@ from .forms import ContactForm
 class IndexView(ListView):
     template_name = 'core/index.html'
     model = Project
+    paginate_by = 10
 
     def get_context_data(self,**kwargs):
         context = super().get_context_data(**kwargs)
